57a8a782dcf1c8a83856897719ac38aee73ced58
vmadmin <vmadmin@webvm-wan.localdomain>
2022-02-10 09:36:25 +0100

    Uebungen-1

---
+++ Uebungen/Uebung1-4/index.php
@@ -0,0 +1,10 @@
+<?php
+    $username = $_GET['username'];
+    
+    echo "Hallo {$username}!<br />";
+
+    if ($_GET['age']) {
+        $age = $_GET['age'];
+        echo "Du bist {$age} Jahre alt.";
+    }
+?>
